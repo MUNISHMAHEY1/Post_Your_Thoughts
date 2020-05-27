4b0b4112b182e6f835fa6c053543b0a5b994bcf7
Munish <munishmahey2@gmail.com>
2020-05-27 06:08:17 -0400

    Developer dependency changed on teh server side

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const resolvers = require('./graphql/resolvers');
-const { MONGODB, PORT } = require('./config');
+const { MONGODB } = require('./config');
 const typeDefs = require('./graphql/typeDefs');
 
 const pubsub = new PubSub();
-
+const PORT = process.env.port || 5000;
 const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -20,5 +20,8 @@ mongoose.connect(MONGODB, {useNewUrlParser: true, useUnifiedTopology: true })
     })
     .then(res => {
         console.log(`Server running at ${res.url}`);
+    })
+    .catch(err => {
+        console.log(err);
     })
 
